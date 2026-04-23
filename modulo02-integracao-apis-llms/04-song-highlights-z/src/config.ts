38f8badc4365be7d70ada50d1bf86b4cf6de06f2
Carlos Henrique Ferreira Gomes <carlos.henriquefg@gmail.com>
2026-04-23 16:31:43 -0300

    Ajustando o modelo

--- modulo02-integracao-apis-llms/04-song-highlights-z/src/config.ts
+++ modulo02-integracao-apis-llms/04-song-highlights-z/src/config.ts
@@ -27,7 +27,7 @@ export const config: ModelConfig = {
   httpReferer: '',
   xTitle: 'IA Devs - Prompt Chaining Article Generator',
   models: [
-    'arcee-ai/trinity-large-preview:free',
+    'nvidia/nemotron-3-super-120b-a12b:free',
   ],
   provider: {
     sort: {
